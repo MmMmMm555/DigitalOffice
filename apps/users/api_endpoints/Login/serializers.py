0d7b7b5ce0c7173d30e50d200a6c5a5d01cbe6f4
MmMmMm555 <qertygfdsa5@gmail.com>
2024-01-03 11:07:09 +0500

    modified:   apps/users/api_endpoints/Login/serializers.py

--- apps/users/api_endpoints/Login/serializers.py
+++ apps/users/api_endpoints/Login/serializers.py
@@ -5,13 +5,19 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         attrs = super().validate(attrs)
+        if self.user.profil:
+            name = self.user.profil.name
+            last_name = self.user.profil.last_name
+        else:
+            name = None
+            last_name = None
         return {
             "id": self.user.id,
             "email": self.user.email,
             "username": self.user.username,
             "role": self.user.role,
-            "first_name": self.user.profil.name,
-            "last_name": self.user.profil.last_name,
+            "first_name": name,
+            "last_name": last_name,
             **attrs,
         }
 
