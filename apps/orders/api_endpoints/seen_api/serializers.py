570b56db3cc64dc44dc3da4a552e9023c0079b43
MmMmMm555 <qertygfdsa5@gmail.com>
2024-01-05 16:30:20 +0500

    modified:   apps/friday_tesis/api_endpoints/seen_api/views.py 	modified:   apps/orders/api_endpoints/seen_api/serializers.py

--- apps/orders/api_endpoints/seen_api/serializers.py
+++ apps/orders/api_endpoints/seen_api/serializers.py
@@ -23,13 +23,16 @@ class DirectionsEmployeeReadSerializer(ModelSerializer):
 class DirectionsEmployeeReadListSerializer(ModelSerializer):
     class Meta:
         model = models.DirectionsEmployeeRead
-        fields = ('id', 'direction', 'employee', 'state', 'requirement', 'created_at',)
+        fields = ('id', 'direction', 'employee',
+                  'state', 'requirement', 'created_at',)
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
+        representation['direction'] = {
+            'id': instance.direction.id, 'title': instance.direction.title, 'direction_type': instance.direction.direction_type, 'types': instance.direction.types, 'from_date': instance.direction.from_date, 'to_date': instance.direction.to_date}
         try:
             representation['employee_name'] = f"{instance.employee.profil.name or None} {instance.employee.profil.last_name or None}"
         except:
-            representation['employee_name'] = None
+            representation['employee_name'] = {instance.employee.username}
         representation['direction_title'] = instance.direction.title
         return representation
