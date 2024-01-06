d4b475e9105854c862b82690a4322f18a5a0e265
MmMmMm555 <qertygfdsa5@gmail.com>
2024-01-06 16:52:53 +0500

    new file:   apps/common/api_endpoints/districts/serializers.py 	new file:   apps/common/api_endpoints/districts/views.py 	modified:   apps/common/api_endpoints/regions/views.py 	modified:   apps/common/urls.py

--- apps/common/api_endpoints/regions/views.py
+++ apps/common/api_endpoints/regions/views.py
@@ -1,21 +1,20 @@
 from rest_framework import generics, parsers
-from django_filters.rest_framework import DjangoFilterBackend
 
-from apps.common.regions import Regions, Districts
-from .serializers import RegionsSerializer, DistrictsSerializer
+from apps.common.regions import Regions
+from .serializers import RegionsSerializer
+
 
 class RegionListView(generics.ListAPIView):
     queryset = Regions.objects.all()
     parser_classes = (parsers.FormParser,)
     serializer_class = RegionsSerializer
-    filter_backends = (DjangoFilterBackend,)
+    search_fields = ('name',)
     filterset_fields = ('id',)
-    
-    
+
+
 # class RegionListAPIView(generics.CreateAPIView):
 #     queryset = Regions.objects.all()
 #     parser_classes = (parsers.FormParser,)
 #     serializer_class = RegionsSerializer
 #     filter_backends = (DjangoFilterBackend,)
 #     filterset_fields = ('id',)
-    
