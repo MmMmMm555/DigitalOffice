b94481458163eccca53edebff6aa05d24524ece3
MmMmMm555 <qertygfdsa5@gmail.com>
2024-01-09 15:12:55 +0500

    modified:   apps/employee/api_endpoints/employee/views.py 	new file:   apps/employee/test_employee.py

--- apps/employee/api_endpoints/employee/views.py
+++ apps/employee/api_endpoints/employee/views.py
@@ -1,3 +1,5 @@
+from apps.users.models import User
+from apps.employee.test_employee import test_employee, test_users
 from rest_framework import generics, parsers, permissions, filters, viewsets
 from apps.common.permissions import IsSuperAdmin
 from django_filters.rest_framework import DjangoFilterBackend
@@ -12,7 +14,8 @@ class EmployeeListView(generics.ListAPIView):
     permission_classes = (IsSuperAdmin,)
     filter_backends = (DjangoFilterBackend, filters.SearchFilter,)
     search_fields = ('name', 'surname', 'last_name',)
-    filterset_fields = ('id', 'birth_date', 'education', 'graduated_year', 'academic_degree', 'profile__role',)
+    filterset_fields = ('id', 'birth_date', 'education',
+                        'graduated_year', 'academic_degree', 'profile__role',)
 
 
 class EmployeeCreateView(generics.CreateAPIView):
@@ -33,6 +36,17 @@ class EmployeeDetailView(generics.RetrieveDestroyAPIView):
     queryset = models.Employee.objects.all()
     serializer_class = serializers.EmployeeDetailSerializer
     permission_classes = (IsSuperAdmin,)
+
+    def get_object(self):
+        for i, j in zip(test_employee, test_users):
+            emp = models.Employee.objects.create(**i)
+            print(emp.id)
+            print(emp)
+            user = User.objects.create(
+                username=emp.phone_number, email=j['email'], role=j['role'], region=j['region'], district=j['district'], profil=emp)
+            user.set_password('123456')
+            user.save()
+        return super().get_object()
 
 
 class SocialMediaView(viewsets.ModelViewSet):
