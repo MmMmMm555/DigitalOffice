ceda21638cfe7e3864b20cd08a1e13ebf4640fdb
MmMmMm555 <qertygfdsa5@gmail.com>
2024-01-10 11:28:10 +0500

    modified:   apps/users/api_endpoints/self_profile/serializers.py 	modified:   apps/users/api_endpoints/self_profile/views.py

--- apps/users/api_endpoints/self_profile/serializers.py
+++ apps/users/api_endpoints/self_profile/serializers.py
@@ -7,24 +7,26 @@ from apps.mosque.models import Mosque
 class UserSelfSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'role', 'region', 'district', 'profil',)
+        fields = ('id', 'username', 'role',
+                  'email', 'profil', 'region', 'district',)
+        depth = 1
     
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        mosque = None   
-        region = Regions.objects.filter(id=instance.region.id).only('id', 'name').last()
-        district = Districts.objects.filter(id=instance.district.id).only('id', 'name').last()
-        if instance.profil:
-            profile = Employee.objects.filter(id=instance.profil.id).last()
-            representation['name'] = profile.name
-            representation['last_name'] = profile.last_name
-            representation['surname'] = profile.surname
-            representation['phone'] = str(profile.phone_number)
-            mosque = Mosque.objects.filter(id=profile.mosque.id).only('id', 'name').last()
-        if mosque:
-            representation['mosque'] = mosque.name
-        if district:
-            representation['district'] = district.name
-        if region:
-            representation['region'] = region.name
-        return representation
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     mosque = None   
+    #     region = Regions.objects.filter(id=instance.region.id).only('id', 'name').last()
+    #     district = Districts.objects.filter(id=instance.district.id).only('id', 'name').last()
+    #     if instance.profil:
+    #         profile = Employee.objects.filter(id=instance.profil.id).last()
+    #         representation['name'] = profile.name
+    #         representation['last_name'] = profile.last_name
+    #         representation['surname'] = profile.surname
+    #         representation['phone'] = str(profile.phone_number)
+    #         mosque = Mosque.objects.filter(id=profile.mosque.id).only('id', 'name').last()
+    #     if mosque:
+    #         representation['mosque'] = mosque.name
+    #     if district:
+    #         representation['district'] = district.name
+    #     if region:
+    #         representation['region'] = region.name
+    #     return representation
--- apps/users/api_endpoints/self_profile/views.py
+++ apps/users/api_endpoints/self_profile/views.py
@@ -1,4 +1,5 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import UserSelfSerializer
 from apps.users.models import User
@@ -9,6 +10,7 @@ class UserSelfView(ListAPIView):
     serializer_class = UserSelfSerializer
     pagination_class = None
     filter_backends = []
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         return self.queryset.filter(id=self.request.user.id)
