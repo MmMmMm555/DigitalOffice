50ef153dec2c61fd00a3f913629fa2b81626410f
MmMmMm555 <qertygfdsa5@gmail.com>
2024-01-10 14:11:42 +0500

    modified:   apps/common/api_endpoints/notifications/views.py 	modified:   apps/common/urls.py

--- apps/common/api_endpoints/notifications/views.py
+++ apps/common/api_endpoints/notifications/views.py
@@ -1,7 +1,19 @@
-from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.decorators import api_view
 
 from apps.orders.models import DirectionsEmployeeRead
-from apps.friday_tesis.models import FridayTesisImamRead
+from apps.friday_tesis.models import FridayTesisImamRead, States
+from rest_framework.response import Response
 
-class NotificationAPI(APIView):
-    pass
+
+# class NotificationApi(GenericAPIView):
+# queryset = DirectionsEmployeeRead.objects.all()
+@api_view(['GET'])
+def NotificationApi(request):
+    directions = DirectionsEmployeeRead.objects.filter(
+        state=States.UNSEEN, employee=request.user).values('id', 'direction__title', 'direction__from_role', 'direction__direction_type', 'created_at',)
+    friday_tesis = FridayTesisImamRead.objects.filter(
+        state=States.UNSEEN, imam=request.user).values('id', 'tesis__title', 'created_at',)
+    data = {'count': directions.count()+friday_tesis.count(),
+            'directions': directions, 'friday_tesis': friday_tesis, }
+    return Response(data=data,)
--- apps/common/urls.py
+++ apps/common/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from apps.common.api_endpoints.regions.views import RegionListView
 from apps.common.api_endpoints.districts.views import DistrictListView
+from apps.common.api_endpoints.notifications.views import NotificationApi
 
 urlpatterns = [
+    # regions api
     path('regions/', RegionListView.as_view(), name='regions_list'),
     path('districts/', DistrictListView.as_view(), name='districts_list'),
+
+    # notifications api
+    path('notifications/', NotificationApi, name='districts_list'),
 ]
