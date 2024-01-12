840536c510e48033b781fabfa129940791b28c4f
MmMmMm555 <qertygfdsa5@gmail.com>
2024-01-12 16:32:09 +0500

    modified:   core/settings/base.py 	modified:   core/settings/production.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -304,13 +304,13 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000 * 10000
 # cors
 CORS_ALLOWED_ORIGINS = [
     'http://45.12.236.79',
-    'https://raqamli-idora.netlify.app/',
+    'https://raqamli-idora.netlify.app',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
-    'https://raqamli-idora.netlify.app/',
+    'https://raqamli-idora.netlify.app',
 )
 CORS_ALLOW_METHODS = [
     "DELETE",
--- core/settings/production.py
+++ core/settings/production.py
@@ -27,40 +27,5 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ###################################################################
 # CORS
 
-CORS_ALLOWED_ORIGINS = [
-    'http://45.12.236.79',
-]
-
 ###################################################################
 
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-# CSRF_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE_ALL = True
-# CORS_ALLOW_ALL_ORIGINS = True
-
-# CORS_ALLOW_CREDENTIALS = False
-
-CORS_ALLOW_METHODS = [
-    "DELETE",
-    "GET",
-    "OPTIONS",
-    "PATCH",
-    "POST",
-    "PUT",
-]
-# CORS_ALLOW_HEADERS = ['*']
-
-# init sentry
-# sentry_sdk.init(
-#     dsn=env.str("SENTRY_DSN", default=""),  # noqa
-#     integrations=[DjangoIntegration(), CeleryIntegration()],
-#     # Set traces_sample_rate to 1.0 to capture 100%
-#     # of transactions for performance monitoring.
-#     # We recommend adjusting this value in production.
-#     traces_sample_rate=1.0,
-#     # If you wish to associate users to errors (assuming you are using
-#     # django.contrib.auth) you may enable sending PII data.
-#     send_default_pii=True,
-# )
-# end init sentry
