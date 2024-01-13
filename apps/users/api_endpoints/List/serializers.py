7577ce46de222c861e6a170c8ace45f95e5851f5
MmMmMm555 <qertygfdsa5@gmail.com>
2024-01-13 14:39:28 +0500

    modified:   apps/users/api_endpoints/List/serializers.py

--- apps/users/api_endpoints/List/serializers.py
+++ apps/users/api_endpoints/List/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 
 from apps.users.models import User
 
@@ -12,6 +13,7 @@ class UsersListSerializer(serializers.ModelSerializer):
     district_name = serializers.CharField(
         source='district.name', read_only=True)
     mosque = serializers.CharField(source='profil.mosque.name', read_only=True)
+
     class Meta:
         model = User
         fields = ('id', 'username', 'name', 'last_name', 'role',
@@ -28,10 +30,21 @@ class UsersDetailSerializer(serializers.ModelSerializer):
 
 
 class UsersUpdateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True, required=False, validators=[validate_password])
+
     class Meta:
         model = User
         fields = ('username', 'role',
-                  'email', 'profil', 'region', 'district',)
+                  'email', 'profil', 'region', 'district', 'password',)
         extra_kwargs = {
             'username': {'required': False},
+            'password': {'required': False},
         }
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+            instance.save()
+        return super().update(instance, validated_data)
