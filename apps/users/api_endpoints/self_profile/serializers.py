41044542397bfa48bca00856cadde23d0c46e053
MmMmMm555 <qertygfdsa5@gmail.com>
2024-01-14 20:15:04 +0500

    modified:   apps/users/api_endpoints/self_profile/serializers.py

--- apps/users/api_endpoints/self_profile/serializers.py
+++ apps/users/api_endpoints/self_profile/serializers.py
@@ -1,19 +1,20 @@
 from rest_framework.serializers import ModelSerializer
 
-from apps.users.models import User, Employee, Districts, Regions
-from apps.mosque.models import Mosque
+from apps.users.models import User
+from apps.employee.api_endpoints.employee.serializers import EmployeeDetailSerializer
 
 
 class UserSelfSerializer(ModelSerializer):
+    profil = EmployeeDetailSerializer(read_only=True)
+
     class Meta:
         model = User
         fields = ('id', 'username', 'role',
                   'email', 'profil', 'region', 'district',)
-        depth = 1
-    
+
     # def to_representation(self, instance):
     #     representation = super().to_representation(instance)
-    #     mosque = None   
+    #     mosque = None
     #     region = Regions.objects.filter(id=instance.region.id).only('id', 'name').last()
     #     district = Districts.objects.filter(id=instance.district.id).only('id', 'name').last()
     #     if instance.profil:
