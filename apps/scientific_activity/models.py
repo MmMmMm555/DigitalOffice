8652239061a4d18d32808cd8649123b588882892
MmMmMm555 <qertygfdsa5@gmail.com>
2024-01-22 19:32:12 +0500

    new file:   apps/scientific_activity/migrations/0003_alter_article_article_types_alter_article_comment_and_more.py 	modified:   apps/scientific_activity/models.py 	new file:   apps/users/migrations/0004_alter_user_district_alter_user_profil_and_more.py 	modified:   apps/users/models.py

--- apps/scientific_activity/models.py
+++ apps/scientific_activity/models.py
@@ -1,80 +1,94 @@
 from django.db import models
 from django.conf import settings
 from django.core.validators import FileExtensionValidator
+from django.utils.translation import gettext_lazy as _
 
+from apps.common.validators import validate_image_size
 from apps.common.models import BaseModel
 from apps.users.models import User
 
 
 class Types(models.TextChoices):
-    ELECTRON = '1'
-    PRINT = '2'
+    ELECTRON = 'electron'
+    PRINT = 'print'
+
 
 class PublicationType(models.TextChoices):
-    NEWSPAPER = '1'
-    JOURNAL = '2'
+    NEWSPAPER = 'newspaper'
+    JOURNAL = 'journal'
+
 
 class ArticleType(models.TextChoices):
-    ISLAM_NURI = '1'
-    HIDAYAT = '2'
-    IMAM_BUKXARIY = '3'
-    MUMINS = '4'
-    OTHER = '5'
+    ISLAM_NURI = 'islam_nuri'
+    HIDAYAT = 'hidayat'
+    IMAM_BUKXARIY = 'imam_bukxariy'
+    MUMINS = 'mumins'
+    OTHER = 'other'
 
 
 class Images(models.Model):
-    image = models.ImageField(upload_to='images/article', validators=[FileExtensionValidator(allowed_extensions=settings.ALLOWED_IMAGE_TYPES)], help_text=f"allowed images: {settings.ALLOWED_IMAGE_TYPES}")
+    image = models.ImageField(verbose_name=_("image"), upload_to='images/article', validators=[FileExtensionValidator(
+        allowed_extensions=settings.ALLOWED_IMAGE_TYPES), validate_image_size], help_text=f"allowed images: {settings.ALLOWED_IMAGE_TYPES}")
 
-    class Meta: 
+    def __str__(self) -> str:
+        return self.image.url
+
+    class Meta:
         verbose_name = 'Maqola rasmi '
         verbose_name_plural = 'Maqola rasmlari '
 
 
 class Article(BaseModel):
-    imam = models.ForeignKey(User, on_delete=models.CASCADE, related_name='imam_articles')
-    type = models.CharField(max_length=22, choices=Types.choices, default=Types.PRINT, blank=False)
-    comment = models.TextField()
-    images = models.ManyToManyField(Images, blank=True)
-    url = models.URLField(max_length=200, null=True, blank=True)
-    publication = models.CharField(max_length=200, blank=True, null=True)
-    publication_type = models.CharField(max_length=10, blank=True, null=True, choices=PublicationType.choices)
-    article_types = models.CharField(max_length=15, blank=True, null=True, choices=ArticleType.choices)
-    date = models.DateField()
-    
+    imam = models.ForeignKey(
+        User, verbose_name=_("imam"), on_delete=models.CASCADE, related_name='imam_articles')
+    type = models.CharField(verbose_name=_("type"), 
+        max_length=22, choices=Types.choices, default=Types.PRINT, blank=False)
+    comment = models.TextField(verbose_name=_("comment"), )
+    images = models.ManyToManyField(Images, verbose_name=_("images"), blank=True)
+    url = models.URLField(verbose_name=_("url"), max_length=200, null=True, blank=True)
+    publication = models.CharField(verbose_name=_("publication"), max_length=200, blank=True, null=True)
+    publication_type = models.CharField(verbose_name=_("publication_type"), 
+        max_length=10, blank=True, null=True, choices=PublicationType.choices)
+    article_types = models.CharField(verbose_name=_("article_types"), 
+        max_length=15, blank=True, null=True, choices=ArticleType.choices)
+    date = models.DateField(verbose_name=_("date"), )
+
     def __str__(self):
         return self.imam.username
 
-    class Meta: 
+    class Meta:
         verbose_name = 'Maqola '
         verbose_name_plural = 'Maqolalar '
 
 
-
 class Direction(models.TextChoices):
-    RELIGIOUS = '1'
-    EDUCATIONAL = '2'
+    RELIGIOUS = 'religious'
+    EDUCATIONAL = 'educational'
 
 
 class BookImages(models.Model):
-    image = models.ImageField(upload_to='images/book', validators=[FileExtensionValidator(allowed_extensions=settings.ALLOWED_IMAGE_TYPES)], help_text=f"allowed images: {settings.ALLOWED_IMAGE_TYPES}")
+    image = models.ImageField(verbose_name=_("image"), upload_to='images/book', validators=[FileExtensionValidator(
+        allowed_extensions=settings.ALLOWED_IMAGE_TYPES), validate_image_size], help_text=f"allowed images: {settings.ALLOWED_IMAGE_TYPES}")
 
-    class Meta: 
+    class Meta:
         verbose_name = 'Kitob rasmi '
         verbose_name_plural = 'Kitob rasmlari '
 
 
 class Book(BaseModel):
-    imam = models.ForeignKey(User, on_delete=models.CASCADE, related_name='imam_books')
-    name = models.CharField(max_length=200)
-    images = models.ManyToManyField(BookImages, blank=True)
-    direction = models.CharField(max_length=22, choices=Direction.choices, default=Direction.RELIGIOUS, blank=False)
-    publication = models.CharField(max_length=200, blank=True, null=True)
-    comment = models.TextField()
-    date = models.DateField()
-    
+    imam = models.ForeignKey(
+        User, verbose_name=_("imam"), on_delete=models.CASCADE, related_name='imam_books')
+    name = models.CharField(verbose_name=_("name"), max_length=200)
+    images = models.ManyToManyField(BookImages, verbose_name=_("images"), blank=True)
+    direction = models.CharField(verbose_name=_("direction"), 
+        max_length=22, choices=Direction.choices, default=Direction.RELIGIOUS, blank=False)
+    publication = models.CharField(verbose_name=_("publication"), max_length=200, blank=True, null=True)
+    comment = models.TextField(verbose_name=_("comment"), )
+    date = models.DateField(verbose_name=_("date"), )
+
     def __str__(self):
         return self.imam.username
 
-    class Meta: 
+    class Meta:
         verbose_name = 'Kitob '
         verbose_name_plural = 'Kitoblar '
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
-from apps.employee.models import Employee
-from apps.common.regions import Regions, Districts
 from django.contrib.auth.hashers import make_password
 
-# Create your models here.
+from apps.employee.models import Employee
+from apps.common.regions import Regions, Districts
 
 
 class Role(models.TextChoices):
@@ -20,13 +19,12 @@ class User(AbstractUser):
     first_name = None
     last_name = None
     region = models.ForeignKey(
-        Regions, on_delete=models.SET_NULL, blank=True, null=True, related_name='region')
+        Regions, verbose_name=_("region"), on_delete=models.SET_NULL, blank=True, null=True, related_name='region')
     district = models.ForeignKey(
-        Districts, on_delete=models.SET_NULL, blank=True, null=True, related_name='district')
-    profil = models.OneToOneField(Employee, unique=True, verbose_name=_(
-        "Profile"), on_delete=models.SET_NULL, related_name='profile', blank=True, null=True)
-    role = models.CharField(
-        max_length=18, choices=Role.choices, default=Role.IMAM, blank=True, null=True)
+        Districts, verbose_name=_("district"), on_delete=models.SET_NULL, blank=True, null=True, related_name='district')
+    profil = models.OneToOneField(Employee, unique=True, verbose_name=_("profile"), on_delete=models.SET_NULL, related_name='profile', blank=True, null=True)
+    role = models.CharField(verbose_name=_("role"),
+                            max_length=18, choices=Role.choices, default=Role.IMAM, blank=True, null=True)
 
     def create_user(self, username, password=None):
         if username is None:
